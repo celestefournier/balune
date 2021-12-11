d55a18be3a57f75188ccdd5f5d8be8f0feb92b6b
Celeste Fournier <gustavo.s.fournier@gmail.com>
2021-12-11 03:03:27 -0300

    Fix Button component

--- Assets/Scripts/UI/Button.cs
+++ Assets/Scripts/UI/Button.cs
@@ -3,28 +3,47 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
 {
     [SerializeField] UnityEvent onClick;
 
+    bool mouseOver;
+    bool pressed;
+
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        transform.DOScale(1.3f, 0.2f);
+        if (!pressed)
+        {
+            mouseOver = true;
+            transform.DOScale(1.3f, 0.2f);
+        }
     }
 
-    public void OnPointerExit(PointerEventData pointerEventData)
+    public void OnPointerExit(PointerEventData pointerEventData = null)
     {
+        mouseOver = false;
         transform.DOScale(1, 0.2f);
     }
 
     public void OnPointerDown(PointerEventData pointerEventData)
     {
+        pressed = true;
         transform.DOScale(1, 0.1f);
     }
 
     public void OnPointerUp(PointerEventData pointerEventData)
     {
-        transform.DOScale(1.3f, 0.1f);
+        if (mouseOver) transform.DOScale(1.3f, 0.1f);
+        pressed = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
         onClick.Invoke();
+    }
+
+    void OnDisable()
+    {
+        OnPointerExit();
     }
 }
